65f88e73ad8221d53b8505d1ffde1a50a8bc14ed
pwrstudio <pwrpntr@gmail.com>
2023-02-15 21:41:57 +0100

    map clustering

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -7,7 +7,7 @@
   import "mapbox-gl/dist/mapbox-gl.css"
 
   const center = { lat: 55.8725675, lon: 13.5645621 }
-  let map
+  let map: mapboxgl.Map
 
   const GOOGLE_API_KEY = "AIzaSyDSzr2hrtfnGyoMrSVk8g7ReY6-t8_1mk8"
 
@@ -25,15 +25,80 @@
     return fetch(requestUrl)
       .then(response => response.json())
       .then(data => {
-        // Check if the request was successful
         if (data.status === "OK") {
-          // Extract the coordinates from the response
-          const coordinates = data.results[0].geometry.location
-          return coordinates
+          return data.results[0].geometry.location
         } else {
           throw new Error(data.error_message)
         }
       })
+  }
+
+  type Feature = {
+    type: "Feature"
+    properties: {
+      title: string
+      slug: string
+    }
+    geometry: {
+      type: "Point"
+      coordinates: [number, number]
+    }
+  }
+
+  type FeatureCollection = {
+    type: "FeatureCollection"
+    features: Feature[]
+  }
+
+  function randomShiftCoordinate(
+    coordinate: [number, number],
+    shiftAmount: number
+  ): [number, number] {
+    // Generate random values for the latitude and longitude shift
+    const latShift = (Math.random() * 2 - 1) * shiftAmount
+    const lngShift = (Math.random() * 2 - 1) * shiftAmount
+
+    // Add the shift values to the original coordinate
+    const newLat = coordinate[1] + latShift
+    const newLng = coordinate[0] + lngShift
+
+    // Return the new coordinate as an array
+    return [newLng, newLat]
+  }
+
+  async function generateGeoJson() {
+    let geoJson: FeatureCollection = {
+      type: "FeatureCollection",
+      features: [],
+    }
+
+    $filteredPosts.forEach(p => {
+      if (p.locationText_sve) {
+        geocodeAddress(p.locationText_sve, GOOGLE_API_KEY)
+          .then(coordinates => {
+            console.log(coordinates)
+            geoJson.features.push({
+              type: "Feature",
+              properties: {
+                title: p.title,
+                slug: p.slug.current ?? "",
+              },
+              geometry: {
+                type: "Point",
+                coordinates: randomShiftCoordinate(
+                  [coordinates.lng, coordinates.lat],
+                  0.0008
+                ),
+              },
+            })
+          })
+          .catch(error => {
+            console.error(error)
+          })
+      }
+    })
+
+    return geoJson
   }
 
   onMount(async () => {
@@ -44,23 +109,84 @@
       style: "mapbox://styles/mapbox/light-v11",
     })
 
-    $filteredPosts.forEach(p => {
-      if (p.locationText_sve) {
-        geocodeAddress(p.locationText_sve, GOOGLE_API_KEY)
-          .then(coordinates => {
-            let el = document.createElement("div")
-            el.className = "marker"
+    const pointsGeoJson = await generateGeoJson()
 
-            new mapboxgl.Marker(el).setLngLat(coordinates).addTo(map)
+    map.on("load", () => {
+      map.addSource("points", {
+        type: "geojson",
+        data: pointsGeoJson,
+        cluster: true,
+        clusterMaxZoom: 14, // Max zoom to cluster points on
+        clusterRadius: 40, // Radius of each cluster when clustering points (defaults to 50)
+      })
 
-            el.addEventListener("click", function () {
-              goto($urlPrefix + "post/" + p.slug?.current)
-            })
-          })
-          .catch(error => {
-            console.error(error)
-          })
-      }
+      map.addLayer({
+        id: "clusters",
+        type: "circle",
+        source: "points",
+        filter: ["has", "point_count"],
+        paint: {
+          "circle-color": [
+            "step",
+            ["get", "point_count"],
+            "#000000",
+            100,
+            "#000000",
+            750,
+            "#000000",
+          ],
+          "circle-radius": [
+            "step",
+            ["get", "point_count"],
+            20,
+            100,
+            30,
+            750,
+            40,
+          ],
+        },
+      })
+
+      map.addLayer({
+        id: "cluster-count",
+        type: "symbol",
+        source: "points",
+        filter: ["has", "point_count"],
+        layout: {
+          "text-field": ["get", "point_count_abbreviated"],
+          "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+          "text-size": 12,
+        },
+        paint: {
+          "text-color": "#ffffff",
+        },
+      })
+
+      map.addLayer({
+        id: "unclustered-point",
+        type: "circle",
+        source: "points",
+        filter: ["!", ["has", "point_count"]],
+        paint: {
+          "circle-color": "#000000",
+          "circle-radius": 14,
+          "circle-stroke-width": 2,
+          "circle-stroke-color": "#ffffff",
+        },
+      })
+
+      map.on("click", "unclustered-point", e => {
+        const slug = e.features[0].properties.slug
+        goto($urlPrefix + "post/" + slug)
+      })
+
+      map.on("mouseenter", "unclustered-point", () => {
+        map.getCanvas().style.cursor = "pointer"
+      })
+
+      map.on("mouseleave", "unclustered-point", () => {
+        map.getCanvas().style.cursor = ""
+      })
     })
   })
 </script>
